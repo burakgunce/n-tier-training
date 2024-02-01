806e48c9aaad1743a11b9d87f289173831b66cb5
burakgunce <brk.gnc9@hotmail.com>
2024-02-02 01:09:50 +0300

    Add project files.

---
+++ MusicMarket.Data/MusicMarketDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicMarket.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicMarket.Data
+{
+    public class MusicMarketDbContext : DbContext
+    {
+        public DbSet<Music> Musics { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        public MusicMarketDbContext(DbContextOptions<MusicMarketDbContext> options) : base(options) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MusicMarket.Data/Repositories/ArtistRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicMarket.Core.Models;
+using MusicMarket.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicMarket.Data.Repositories
+{
+    public class ArtistRepository : Repository<Artist>, IArtistRepository
+    {
+        public ArtistRepository(MusicMarketDbContext context) : base(context)
+        {
+
+        }
+
+        public async Task<IEnumerable<Artist>> GetAllWithMusicAsync()
+        {
+            return await MusicMarketDbContext.Artists.Include(x => x.Musics).ToListAsync();
+        }
+
+        public Task<Artist> GetWithMusicByIdAsync(int id)
+        {
+            return MusicMarketDbContext.Artists.Include(x => x.Musics).SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        private MusicMarketDbContext MusicMarketDbContext
+        {
+            get { return Context as MusicMarketDbContext; }
+            //bu bi property inherit aldıgın yerdeki context i buna çeviriyo
+        }
+    }
+}
